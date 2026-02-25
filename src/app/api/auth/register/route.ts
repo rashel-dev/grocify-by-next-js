210083243306b1239587d744e89d039e3abf00df
rashel9255 <ibrahim2000167@gmail.com>
2026-02-26 00:35:51 +0600

    refactor: improve the register api. - name, email, password check -password hashed -send only neccessary data in response. password dont send for security

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,28 +1,60 @@
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
-        await dbConnect();
         const { name, email, password } = await req.json();
+
+        if(!name || !email || !password){
+            return NextResponse.json({
+                success: false,
+                message: "All fields are required",
+            }, {status: 400})
+        }
+
+        const normalizedEmail = email.trim().toLowerCase();
+
+        await dbConnect();
+
+        const existingUser = await User.findOne({email:normalizedEmail});
+
+        if(existingUser) {
+            return NextResponse.json({
+                success: false,
+                message: "User already exists",
+            }, { status: 400})
+        }
         
+        if(password.length < 6) {
+            return NextResponse.json({
+                success: false,
+                message: "Password must be at least 6 character long",
+            }, { status: 400})
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({
             name,
-            email,
-            password
+            email:normalizedEmail,
+            password:hashedPassword
         })
 
         return NextResponse.json({
             success: true,
             message: "User registered successfully",
-            user: newUser
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            }
         }, {status: 201})
     } catch (error) {
+        console.error(error);
         return NextResponse.json({
             success: false,
             message: "Failed to register user",
-            error: error
         }, {status: 500})
     }
 }
