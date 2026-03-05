41ebec00ef52f7965c13fe02d2eb5402e9a65729
rashel9255 <ibrahim2000167@gmail.com>
2026-03-06 01:45:35 +0600

    feat: add NextAuth session provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Provider from "./Provider";
 
 export const metadata: Metadata = {
-  title: "Grocify",
-  description: "Grocify - Your one-stop shop for all your grocery needs",
+    title: "Grocify",
+    description: "Grocify - Your one-stop shop for all your grocery needs",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-linear-to-b from-green-100 to-white min-h-screen w-full">
-        {children}
-        <Toaster position="top-center" />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className="bg-linear-to-b from-green-100 to-white min-h-screen w-full">
+                <Provider>
+                    {children}
+                    <Toaster position="top-center" />
+                </Provider>
+            </body>
+        </html>
+    );
 }
