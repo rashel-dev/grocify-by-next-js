3d7dd5f5a4fad0afffc34c2815d63aed729319d3
rashel9255 <ibrahim2000167@gmail.com>
2026-03-17 03:28:04 +0600

    feat: project update

--- src/auth.ts
+++ src/auth.ts
@@ -93,5 +93,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60, // 30 days
     },
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
 });
--- src/proxy.ts
+++ src/proxy.ts
@@ -12,7 +12,7 @@ export async function proxy(request: NextRequest) {
 
     const token = await getToken({ 
         req: request, 
-        secret: process.env.AUTH_SECRET 
+        secret: process.env.NEXTAUTH_SECRET 
     });
 
     if(!token) {
