171799318dcf59b5e1e66f2feb8b71177ff3f75a
rashel9255 <ibrahim2000167@gmail.com>
2026-03-19 22:43:27 +0600

    feat: in homepage onclick on the profile image will show profile

--- src/Components/Home/Navbar.tsx
+++ src/Components/Home/Navbar.tsx
@@ -1,37 +1,34 @@
 import { User } from 'next-auth';
-import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 import { LuSearch } from 'react-icons/lu';
+import ProfileSection from './ProfileSection';
 
 const Navbar = ({user}: {user: User}) => {
+
+    const plainUser = JSON.parse(JSON.stringify(user));
     
     return (
-        <div className='w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-16 px-4 md:px-8 z-50'>
+        <div className="w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-16 px-4 md:px-8 z-50">
+            <Link href={"/"} className="text-white font-bold text-2xl tracking-wide hover:text-white/80 transition-colors duration-300">
+                Grocify
+            </Link>
 
-            <Link href={'/'} className='text-white font-bold text-2xl tracking-wide hover:text-white/80 transition-colors duration-300'>Grocify</Link>
-
-            <form className='hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md'>
-                <LuSearch className='text-gray-500 w-5 h-5 mr-2'/>
-                <input type="text" placeholder='Search products...' className='outline-none w-full text-gray-700 placeholder:text-gray-400' />
+            <form className="hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md">
+                <LuSearch className="text-gray-500 w-5 h-5 mr-2" />
+                <input type="text" placeholder="Search products..." className="outline-none w-full text-gray-700 placeholder:text-gray-400" />
             </form>
 
-            <div className='flex items-center gap-3 md:gap-6 relative'> 
-                <Link href={'/cart'}>
-                    <div className='relative'>
-                        <FaShoppingCart className='text-white w-6 h-6 hover:text-white/80 transition-colors duration-300' />
-                        <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center'>0</span>
+            <div className="flex items-center gap-3 md:gap-6 relative">
+                {/* Cart icon */}
+                <Link href={"/cart"}>
+                    <div className="relative">
+                        <FaShoppingCart className="text-white w-6 h-6 hover:text-white/80 transition-colors duration-300" />
+                        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">0</span>
                     </div>
                 </Link>
 
-                <div>
-                    {user.image ? (
-                        <Image src={user.image} alt={user.name} width={40} height={40} className='rounded-full' />
-                    ) : (
-                        <FaUser className='text-white w-6 h-6 hover:text-white/80 transition-colors duration-300' />
-                    )}
-                </div>
+                <ProfileSection user={plainUser} />
             </div>
         </div>
     );
---
+++ src/Components/Home/ProfileSection.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { AnimatePresence } from "motion/react";
+import { User } from "next-auth";
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { LuPackage } from "react-icons/lu";
+import { IoIosLogOut } from "react-icons/io";
+
+const ProfileSection = ({ user }: { user: User }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="relative">
+            {/* User Profile icon */}
+            <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+                {user.image ? (
+                    <Image src={user.image} alt={user.name || "User"} width={40} height={40} className="rounded-full" />
+                ) : (
+                    <FaUser className="text-white w-6 h-6 hover:text-white/80 transition-colors duration-300" />
+                )}
+            </div>
+
+            {/* Dropdown menu */}
+
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                        transition={{ duration: 0.6 }}
+                        className="absolute right-0 mt-2 bg-white rounded-lg shadow-lg px-2 py-2 border border-gray-200 z-50 w-56"
+                    >
+                        {/* user info */}
+                        <div className="flex items-center gap-2 px-2 py-2">
+                            <div>
+                                {user.image ? (
+                                    <Image src={user.image} alt={user.name || "User"} width={40} height={40} className="rounded-full" />
+                                ) : (
+                                    <FaUser className="text-white w-6 h-6 hover:text-white/80 transition-colors duration-300" />
+                                )}
+                            </div>
+                            <div className="">
+                                <p className="text-gray-700 font-medium"> {user.name}</p>
+                                <p className="text-gray-500 text-sm">{user.role}</p>
+                            </div>
+                        </div>
+
+                        {/* hr line */}
+                        <div className="border-t border-gray-200 my-2"></div>
+
+                        {/* My Orders */}
+                        <Link href={""} className="flex items-center gap-2 px-3 py-2 text-gray-700 hover:bg-green-50 rounded-md">
+                            <LuPackage className="text-green-700 w-6 h-6" />
+                            <p className="text-green-700 font-medium">My Orders</p>
+                        </Link>
+
+                        {/* logout button */}
+                        <button className="flex items-center gap-2 px-3 py-2 text-gray-700 hover:bg-red-50 w-full cursor-pointer">
+                            <IoIosLogOut className="text-red-700 w-6 h-6" />
+                            <p className="text-red-700 font-medium">Logout</p>
+                        </button>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
+    );
+};
+
+export default ProfileSection;
